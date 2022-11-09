3b7d0bd7c8e95a00f192b08acc5ee53ef0736539
Md Alamgir Hossen <alamgir6934@gmail.com>
2022-11-09 13:08:41 +0600

    basic-mongo-setup

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -7,6 +10,17 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5ia6zmg.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 
 app.get('/', (req, res) => {
