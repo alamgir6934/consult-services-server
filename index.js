3069655610777af65aebd16e3e200f777bdd3375
Md Alamgir Hossen <alamgir6934@gmail.com>
2022-11-09 15:55:57 +0600

    server-side-data-loaded

--- index.js
+++ index.js
@@ -15,11 +15,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5ia6zmg.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('consultService').collection('services');
+
+        //get servies api
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service)
+
+        });
+
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 
 
