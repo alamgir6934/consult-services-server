8821155ea18b5d5087375e9f4c9a0d547255961b
Md Alamgir Hossen <alamgir6934@gmail.com>
2022-11-10 02:31:42 +0600

    delete-review data-added

--- index.js
+++ index.js
@@ -57,6 +57,15 @@ async function run() {
             res.send(result);
         });
 
+        //delete review data
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
 
